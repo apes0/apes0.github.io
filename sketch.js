afb6aad9bbd720754e0c9a1fd769087013b2a304
apes0 <54032987+apes0@users.noreply.github.com>
2019-12-13 22:02:37 +0200

    fr

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textFont('Raleway')
+  textSize(32)
+  text('yay',200,200)
+  textFont('Lato')
+  textSize(32)
+  text('yay',300,200)
+  textFont('Muli')
+  textSize(32)
+  text('yay',300,300)
+}
