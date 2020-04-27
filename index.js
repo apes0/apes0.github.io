35e972728cf2c350ca1a5681cb95a886050da3db
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-27 20:19:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-let t = document.getElementById("p");
-
-function writeDoc(n){
-    for(let i = 0; i < n; i++){
-        loadDoc(i.stringify + '.txt')
-    }
-}
-
-var rawFile = new XMLHttpRequest();
-
-function loadDoc(file){
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                allText = rawFile.responseText;
-                if(file === 'pn.txt'){
-                    writeDoc(parseInt(allText, 10))
-                } else {
-                    t.innerHTML += allText
-                }
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-loadDoc('pn.txt')
+fetch('http://example.com/movies.json')
+  .then((r) => {
+    alert(r.stringify);
+  })
