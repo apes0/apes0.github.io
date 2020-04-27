803485dc5404af08c9114d4df0d8822f34ad086c
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-27 22:06:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 t = document.getElementById('p')
 
 function lp(n){
-  alert(2)
   for(let i = 0; i < n; i ++){
+    alert(i.toString(1) + '.txt')
     read(i.toString(10) + '.txt')
   }
 }
 
 async function read(f) {
-  alert(1)
   const res = await fetch(f);
   let data = await res.json();
   if(f === 'pn.txt'){
