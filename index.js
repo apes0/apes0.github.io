df5a1e2dcf54b8f084cb818ceeaff4b7c5165c32
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-27 23:20:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 t = document.getElementById('p')
 
-function lp(n){
-  for(let i = 0; i < n; i ++){
-    read((i + 1).toString(10) + '.txt')
-  }
-}
-
 async function read(f) {
   const res = await fetch(f);
   let data = await res.json();
-  if(f === 'pn.txt'){
-    lp(parseInt(data, 10))
-  } else {
-    t.innerHTML += data;
-  }
+  return data;
 }
 
-read('pn.txt')
+for(let i = 1; i > 0; i ++){
+  try{
+    p = read(i.toString(10) + '.txt')
+    t.innerHTML += p + '<br>'
+  } catch(err) {
+    break
+  }
