da178fe51112b4fb6d48c4c6c46ea2b9082131e8
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-27 23:39:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ async function read(f) {
   return data;
 }
 
-for(let i = 1; i > 0; i ++){
+for(let i = 1; i < 10; i ++){
   try{
     p = read(i.toString(10) + '.txt')
     t.innerHTML += p + '<br>'
   } catch(err) {
+    alert('error')
     break
   }
 }
