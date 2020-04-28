e941ae810391e83ee9b955634a8cc89d15af710c
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-28 14:16:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 t = document.getElementById('p');
 
+p = {}
+
 function lp(n){
   for(let i = n; i > 0; i --){
     read(i.toString(10) + '.txt');
     var l = Object.keys(p).length;
+    alert(l)
     for(let i = 0; i < l; i ++){
       t.innerHTML += p[(i + 1).stringify()];
     }
   }
 }
-
-p = {}
 
 async function read(f) {
   const res = await fetch(f);
