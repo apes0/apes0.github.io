ad9432625478cab9827ff2e4f6697397f1c54f1b
apes0 <54032987+apes0@users.noreply.github.com>
2020-04-29 15:45:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ t = document.body;
 
 async function lp(){
   for(let i = 1; i > 0; i ++){
-    await read(i.toString(10) + '.txt');
+    try {
+      await read(i.toString(10) + '.txt');
+    } catch {
+      break;
+    }
   }
 }
 
 async function read(f) {
-  try {
-    const res = await fetch(f);
-    let data = await res.json();
-    t.innerHTML += data;
-  } catch {
-    return;
-  }
+  const res = await fetch(f);
+  let data = await res.json();
+  t.innerHTML += data;
 }
