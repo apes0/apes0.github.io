0e89231ad045e263fdcfacb474bd96b1621bb92c
apes0 <54032987+apes0@users.noreply.github.com>
2020-05-01 14:22:39 +0300

    Update view.js

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 f = window.location.href.replace(window.location.hostname + '/view?', '');
 
 async function gf(f) {
-  const rel = await fetch(f);
+  const res = await fetch(f);
   let data = await res.json();
   document.body.innerHTML += data;
 }
