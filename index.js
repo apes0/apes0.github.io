cbb0f517cdad805301d6435f7bc91709096f85f1
apes0 <54032987+apes0@users.noreply.github.com>
2020-05-04 15:05:57 +0300

    json update :/

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ async function lp() {
 async function read(f) {
   const res = await fetch(f);
   let data = await res.json();
-  if(data.length > mchr) {
-    data = data.slice(0, mchr) + '...';
-    data += '<button onClick = "view( f )">read more</button>'.replace(' f ', "'" + f + "'");
+  let t = data['title']
+  let p = data['text']
+  if(p.length > mchr){
+    p = p.slice(0, mchr) + '...<button onClick = "view( f )">View more</button>'.replace(' f ', "'" + f + "'")
   }
-  t.innerHTML = data + '<br>'+ t.innerHTML;
+  let pg = '<h1>' + t + '</h1><p>' + p + '</p>'
+  t.innerHTML = pg + '<br>'+ t.innerHTML;
 }
-
-lp()
 
 function view(p) {
   window.location.href = window.location.hostname + '/view' + '?' + p;
 }
+
+lp()
